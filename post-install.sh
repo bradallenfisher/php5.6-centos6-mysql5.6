c34d37263f2c575af976d836ab4b079f415fee47
Brad Fisher <bradallenfisher@gmail.com>
2015-09-02 14:43:42 -0400

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -5,6 +5,7 @@ export PATH="$HOME/.composer/vendor/bin:$PATH"
 
 # install drush recipes
 drush dl drush_recipes -y
+sudo chown -R vagrant:vagrant /var/www/
 drush dl drupal --destination=/var/www/ --drupal-project-rename=html -y
 
 cd /var/www/html
