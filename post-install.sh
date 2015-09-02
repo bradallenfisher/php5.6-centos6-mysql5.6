84d238933bde0e6275ebcf11725796ca0ce0e696
Brad Fisher <bradallenfisher@gmail.com>
2015-09-02 14:46:00 -0400

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -5,13 +5,19 @@ export PATH="$HOME/.composer/vendor/bin:$PATH"
 
 # install drush recipes
 drush dl drush_recipes -y
+drush dl drush_cleanup
+drush cc drush
+
 sudo chown -R vagrant:vagrant /var/www/
 drush dl drupal --destination=/var/www/ --drupal-project-rename=html -y
 
 cd /var/www/html
 drush site-install --db-url=mysql://root@localhost:22/test -y
-chmod -R 755 /var/www/html/sites/default/files/
-chown -R apache:apache /var/www/html/sites/default/files/
+sudo chmod -R 755 /var/www/html/sites/default/files/
+sudo chown -R apache:apache /var/www/html/sites/default/files/
+
+drush cleanup -y
+drush cook d7adminux -y
 exec bash
 
 echo "DUNZY!"
